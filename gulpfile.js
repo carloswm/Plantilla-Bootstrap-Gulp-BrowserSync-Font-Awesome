728a1fc7ac5d71b2dc0977032bfa2d7be9f3e1f6
Carlos Marigorda <carlos@reevolution.com.mx>
2018-07-19 01:00:53 -0500

    first commit

---
+++ gulpfile.js
@@ -0,0 +1,52 @@
+var gulp        = require('gulp');
+var browserSync = require('browser-sync').create();
+var sass        = require('gulp-sass');
+
+// Static Server + watching scss/html files
+gulp.task('serve', ['sass'], function() {
+
+    browserSync.init({
+        server: "./src"
+    });
+
+    gulp.watch([
+      'node_modules/bootstrap/scss/bootstrap.scss',
+      'src/scss/*.scss'
+    ], ['sass']);
+
+    gulp.watch("src/*.html").on('change', browserSync.reload);
+});
+
+// Compile sass into CSS & auto-inject into browsers
+gulp.task('sass', ()=> {
+    return gulp.src([
+      'node_modules/bootstrap/scss/bootstrap.scss',
+      'src/scss/*.scss'
+    ])
+    .pipe(sass({outputStyle: 'compressed'}))
+    .pipe(gulp.dest('src/css'))
+    .pipe(browserSync.stream());
+});
+
+gulp.task('js', () => {
+  return gulp.src([
+    'node_modules/bootstrap/dist/js/bootstrap.min.js',
+    'node_modules/jquery/dist/jquery.min.js',
+    'node_modules/popper.js/dist/umd/popper.min.js'
+  ])
+  .pipe(gulp.dest('src/js'))
+  .pipe(browserSync.stream());
+});
+
+gulp.task('font-awesome', () => {
+  return gulp.src('node_modules/font-awesome/css/font-awesome.min.css')
+  .pipe(gulp.dest('src/css'));
+});
+
+gulp.task('fonts', () => {
+  return gulp.src('node_modules/font-awesome/fonts/*')
+  .pipe(gulp.dest('src/fonts'));
+})
+
+
+gulp.task('default', ['js', 'serve', 'font-awesome', 'fonts']);
